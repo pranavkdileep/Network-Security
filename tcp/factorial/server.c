8329995f95639b4f5cd2fff8250421425f317368
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2024-11-06 18:20:28 +0000

    add

--- tcp/factorial/server.c
+++ tcp/factorial/server.c
@@ -21,7 +21,7 @@ int main()
     struct sockaddr_in serveraddr,clientaddr;
     serveraddr.sin_family=AF_INET;
     serveraddr.sin_addr.s_addr=htonl(INADDR_ANY);
-    serveraddr.sin_port=htons(8891);
+    serveraddr.sin_port=htons(8892);
     sd = socket(AF_INET,SOCK_STREAM,0);
     bind(sd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
     listen(sd,5);
@@ -34,9 +34,9 @@ int main()
         recv(lsd,buffer,sizeof(buffer),0);
         fact(buffer);
         printf("Client :%s\n",buffer);
-        strcpy(buffer,"");
+        //strcpy(buffer,"");
         // printf("Sever :");
-        // fgets(buffer,sizeof(buffer),stdin);
+        //fgets(buffer,sizeof(buffer),stdin);
         send(lsd,buffer,100,0);
     }
 
